b39075bceeb5d796f897824c2bc2c3c9397d796c
Tzuhan <tzuhan@Mac.mymeshdevice.home>
2025-02-20 23:01:15 +0800

    Add next.config.js for GitHub Pages deployment

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/mowfun-test',
+  assetPrefix: '/mowfun-test',
+};
+
+module.exports = nextConfig;
+
